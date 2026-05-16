9ad37079cdcd1bea09324665099d4d38debdf60e
dishaa <disharavi286@gmail.com>
2026-05-16 10:39:47 +0530

    feat: reviews model, sentiment analysis, listings CRUD, Cloudinary upload

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import destinationRoutes from './routes/destinationRoutes.js';
 import itineraryRoutes from './routes/itineraryRoutes.js';
 import listingRoutes from './routes/listingRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
+import reviewRoutes from './routes/reviewRoutes.js';
 
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
@@ -32,6 +33,7 @@ app.use('/api/destinations', destinationRoutes);
 app.use('/api/itinerary', itineraryRoutes);
 app.use('/api/listings', listingRoutes);
 app.use('/api/upload', uploadRoutes);
+app.use('/api/reviews', reviewRoutes);
 
 // Health check
 app.get('/', (req, res) => {
